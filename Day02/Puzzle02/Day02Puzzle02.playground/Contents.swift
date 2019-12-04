bd4db39d04e9a5632062c54619e3c60a982f5710
Manish Rathi <m.rathi@catawiki.nl>
2019-12-04 18:39:10 +0100

    Day02 Puzzle 02 - Solved.

--- Day02/Puzzle02/Day02Puzzle02.playground/Contents.swift
+++ Day02/Puzzle02/Day02Puzzle02.playground/Contents.swift
@@ -14,27 +14,45 @@ enum Opcode: Int64 {
     }
 }
 
-var inputs = input.replacingOccurrences(of: "\n", with: "")
-    .components(separatedBy: ",")
-    .filter { $0 != "" }
-    .map{ Int64($0)! }
-
-//To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
-inputs[1] = 12
-inputs[2] = 2
-
-let forwardRange = 4
-var currentPosition = 0
-while currentPosition + forwardRange < inputs.count  {
-    let opCode = Opcode(rawValue: inputs[currentPosition])!
-    let firstIndex = Int(inputs[currentPosition+1])
-    let secondIndex = Int(inputs[currentPosition+2])
-    guard let result = opCode.performOperation(inputs[firstIndex], inputs[secondIndex]) else { break }
-
-    let outputIndex = Int(inputs[currentPosition+3])
-    inputs[outputIndex] = result
-
-    currentPosition += forwardRange
+var programInputs: [Int64] {
+    input.replacingOccurrences(of: "\n", with: "")
+        .components(separatedBy: ",")
+        .filter { $0 != "" }
+        .map{ Int64($0)! }
 }
 
-print(inputs)
+
+func run(with programInputs: [Int64]) -> Int64 {
+    let forwardRange = 4
+    var inputs = programInputs
+    var currentPosition = 0
+    while currentPosition + forwardRange < inputs.count  {
+        let opCode = Opcode(rawValue: inputs[currentPosition])!
+        let firstIndex = Int(inputs[currentPosition+1])
+        let secondIndex = Int(inputs[currentPosition+2])
+        guard let result = opCode.performOperation(inputs[firstIndex], inputs[secondIndex]) else { return inputs[0] }
+
+        let outputIndex = Int(inputs[currentPosition+3])
+        inputs[outputIndex] = result
+
+        currentPosition += forwardRange
+    }
+
+    return inputs[0]
+}
+
+
+for index in 0...99 {
+    for index2 in 0...99 {
+        var inputs = programInputs
+        inputs[1] = Int64(index)
+        inputs[2] = Int64(index2)
+
+        let output = run(with: inputs)
+
+        if output == 19690720 {
+            print(100 * index + index2)
+            exit(0)
+        }
+    }
+}
