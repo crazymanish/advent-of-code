dc8a5a4c60bb0d77fe41679fbc1da78f3fd1c153
Manish Rathi <m.rathi@catawiki.nl>
2020-12-01 14:13:16 +0100

    Day01 Puzzle01 (improved)

--- 2020/Day01/Puzzle01/Day01Puzzle01.playground/Contents.swift
+++ 2020/Day01/Puzzle01/Day01Puzzle01.playground/Contents.swift
@@ -4,19 +4,22 @@ let numbers = input.components(separatedBy: "\n")
     .filter { $0 != "" }
     .map{ Int($0)! }
 
-print(numbers)
 
-var pairs: [Int: Int] = [:]
-var output = 0
+func findPairs(for input: Int, from numbers: [Int]) -> (Int, Int) {
+    var pairs: [Int: Int] = [:]
 
-for number in numbers {
-    pairs[number] = number
-    let remainingNumber = 2020 - number
-    
-    if let oldNumber = pairs[remainingNumber], (oldNumber + number) == 2020 {
-        output = remainingNumber * number
-        break
+    for number in numbers {
+        pairs[number] = number
+        let remainingNumber = 2020 - number
+        
+        if let oldNumber = pairs[remainingNumber], (oldNumber + number) == 2020 {
+            return (oldNumber, number)
+        }
     }
+    
+    fatalError("this should not expected.")
 }
 
+let pairs = findPairs(for: 2020, from: numbers)
+let output = pairs.0 * pairs.1
 print(output)
